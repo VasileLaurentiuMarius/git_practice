b0447f71789757fc931adeecad1e846e9e04018e
Jukes <vasilelaur637@gmail.com>
2024-08-25 14:50:28 +0300

    finished

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello world')
+let greetings =['Hi ','Heya ','Sup ']
+let names = ['man ','dude ', 'rascal ']
+let request = ['you got some money?','what have you been up to?','hows your day been?']
+function generate(){
+    return greetings[Math.floor(Math.random()*3)] + names[Math.floor(Math.random()*3)] + request[Math.floor(Math.random()*3)]
+}
+console.log(generate())
